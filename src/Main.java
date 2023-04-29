9a4ffd6355ef6a8f2a6d8539a299534e7479221b
russtack <stirlingest@gmail.com>
2023-04-29 17:42:39 -0400

    new

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
+import java.awt.*;
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // IntelliJ IDEA suggests fi
+        Point p1= new Point(1,2);
+        Point p2=p1;
+        System.out.println(p2);
+        p1.x = 3;
+        System.out.println(p2);
+        p2.x=55;
+        System.out.println(p1);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
 
-            long num = 5000_000_000L;
-            float num1 = 0.92356897456f;
-            System.out.println(num);
-
-        }
     }
 }
