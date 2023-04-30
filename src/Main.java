e6afd4b4da84bcce93862f46940ecaea65c60780
russtack <stirlingest@gmail.com>
2023-04-30 15:57:19 -0400

    new

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -8,20 +9,30 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fi
-        String message = new String("aewiwrat");
-        int [] nums = {10,2,5,8,9};
-        Arrays.sort(nums);
-        Arrays.toString(nums);
-
-        int[][] matrix = {{4,5,6},{1,2,3}};
 
 
 
-        System.out.println(Arrays.deepToString(matrix));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.print("Months: ");
+        int months = scanner.nextInt();
+
+        System.out.print("Interest: ");
+        float interest = scanner.nextFloat()/100/12;
+
+        double result = (principal*interest*Math.pow((1+interest),months)/(Math.pow((1+interest),months)-1));
+
+        System.out.println("Yr monthly payeoumount is "+result);
+
+
+
+
 
 
 
 
 
     }
+
 }
